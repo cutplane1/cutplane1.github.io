a6aa7cda5704d762ecebfb9e224ee707555e9d35
cutplane1 <caramelcherry@proton.me>
2026-08-08 19:55:06 +0500

    boop

---
+++ webmote/server.js
@@ -0,0 +1,19 @@
+Deno.serve((req) => {
+
+  if (req.headers.get("upgrade") != "websocket") {
+    return new Response(null, { status: 426 });
+  }
+
+  const { socket, response } = Deno.upgradeWebSocket(req);
+
+  socket.addEventListener("open", () => {
+    console.log("a client connected!");
+  });
+
+  socket.addEventListener("message", (event) => {
+    console.log("Received message:", event.data);
+  });
+
+  return response;
+});
+
